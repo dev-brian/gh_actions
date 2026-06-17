4cc0f1af63e9402cd59efc9ca834107253ef6705
Brian <dev-brian@gmail.com>
2026-06-17 15:38:41 -0600

    path

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-print("Hello World")
+from ast import main
+import os
+
+print("Hello World! runs on Actions")
 for i in range(10):
     print("XD" * i)
+
+    name = os.getenv("USERNAME")
+    print(f"Hello {name}!")
+
+if __name__ == "__main__":
+    main()
